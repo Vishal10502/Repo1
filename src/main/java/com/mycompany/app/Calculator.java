d5ff96382dd5158a4a02d3bfdec650a20d1596f4
vishal10502 <kvishal.10502@gmail.com>
2023-08-22 11:41:07 +0530

    v12

--- src/main/java/com/mycompany/app/Calculator.java
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -22,7 +22,7 @@ public class Calculator
   }
   public static void main( String[] args )
   {
-    System.out.println(Calculator.add(3, 3)); 
-    System.out.println(Calculator.add(3, 3)); 
+    System.out.println(Calculator.add(3, 3));
+    System.out.println(Calculator.add(4, 3))
   }
   }
